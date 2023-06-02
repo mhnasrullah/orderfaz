3a7be8b4b51f945ec81ba7f443bc2ee0e6719cdb
Hasan Nasrullah <hnasrul22@gmail.com>
2023-06-03 06:05:59 +0700

    [INIT] installing nuxt3,tailwindCss,pinia

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ['~/assets/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    modules : [
+        '@pinia/nuxt'
+    ]
+})
+
